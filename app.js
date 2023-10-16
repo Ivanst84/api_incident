c925f567d9b2eae2d8949afea027867baf41ddc2
Ivanst84 <77261169+Ivanst84@users.noreply.github.com>
2023-10-15 22:10:04 -0700

    error en cors arreglado

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const cors = require("cors");
-app.use(cors());
 const app=express();
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 module.exports =app;
